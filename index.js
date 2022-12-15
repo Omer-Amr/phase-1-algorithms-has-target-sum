ba82931ad2a69462ef083efa3f93fe6680256f32
omer <you@example.com>
2022-12-15 19:09:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+ let isTrue = false
+
+ for(i = 0; i < array.length; i++ ){
+  for(j= i+1; j < array.length; j++){
+    if(array[i] + array[j] === target){
+      isTrue = true
+    }
+  }
 }
+
+return isTrue
+}
+
+console.log(hasTargetSum([4, 5, 9, 6, 13, 7], 15))
+console.log(hasTargetSum([2, 6, 23, 6, 14, 30], 100))
+
 
 /* 
   Write the Big O time complexity of your function here
